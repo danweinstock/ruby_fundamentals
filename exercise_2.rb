0ad2ba79839fbd5f37af2888e5f769eaee4bd812
danweinstock <dan.weinstock@yahoo.com>
2015-01-20 14:32:24 -0500

    final exercsie 2 file

--- exercise_2.rb
+++ exercise_2.rb
@@ -5,3 +5,8 @@ puts "The tip for your meal is $#{tip}"
 string = "Happy!"
 integer = 65.to_s
 puts string + integer
+
+big_mult = 45628 * 7839
+puts big_mult.to_s
+
+puts (true && false) || (false && true) || !(false && false)
